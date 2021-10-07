47204db6ebfbef9eefe86a9545d45045f7f9e3d8
Rasha Saleh <rasha.saleh@student.ie.edu>
2021-10-07 16:35:06 +0200

    added func3

--- Session2.py
+++ Session2.py
@@ -6,3 +6,13 @@ Created on Wed Oct  6 10:37:21 2021
 @author: rashasaleh
 """
 
+#%% 
+
+def func (a, b):
+    return a + b 
+
+def func2 (a, b):
+    return a * b 
+
+def func3 (a):
+    return a + 3 
